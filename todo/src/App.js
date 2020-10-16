b5dbb659388ae50e1db193a48943d44d5048e13c
Mirwes <mirwes210@gmail.com>
2020-10-16 10:40:09 -0700

    reducer-todo-project completed

---
+++ todo/src/App.js
@@ -0,0 +1,27 @@
+import React, { useReducer } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import todoReducer, { ADD_TASK, REMOVE_COMPLETED, REMOVE_TASK, TOGGLE_COMPLETE, initialState } from './reducers/todoReducer';
+import TodoForm from './component/TodoForm';
+import TodoList from './component/TodoList';
+import './styling/Todo.css';
+
+
+function App() {
+  const [{ todoList }, dispatch] = useReducer(todoReducer, initialState);
+
+  return (
+    <div className='container' >
+    <header>
+      <h2>TO-DO LIST</h2>
+      {/* Add functionality to hide/show input when clicked (also change icon) */}
+      <button id="toggleInputBtn"><FontAwesomeIcon icon={faPlus} /></button>
+    </header>
+    <TodoForm dispatch={dispatch} ADD_TASK={ADD_TASK} />
+    <TodoList todoList={todoList} dispatch={dispatch} REMOVE_TASK={REMOVE_TASK} TOGGLE_COMPLETE={TOGGLE_COMPLETE} />
+    <button className='clearButton' onClick={() => dispatch({type: REMOVE_COMPLETED})} >Clear Completed</button>
+  </div>
+  );
+}
+
+export default App;
---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,50 @@
+export const TOGGLE_COMPLETE = 'TOGGLE_COMPLETE';
+export const ADD_TASK = 'ADD_TASK';
+export const REMOVE_TASK = 'REMOVE_TASK';
+export const REMOVE_COMPLETED = 'REMOVE_COMPLETED';
+
+export const initialState = {
+
+       todoList: [
+           {
+               task: 'Example Task 1',
+               id: 1,
+               completed: false
+           },
+           {
+               task: 'Example Task 2',
+               id: 2,
+               completed: false
+           }
+       ]
+    }
+
+    export default (state=initialState, action) => {
+        switch (action.type) {
+        case 'TOGGLE_COMPLETE':
+            return {todoList: state.todoList.map(task => {
+                if (task.id === action.payload) {
+                    return {
+                        ...task,
+                        completed: !task.completed
+                    };
+                } else {
+                    return task;
+                }
+            })};
+            case 'ADD_TASK':
+
+                 return {todoList: [...state.todoList, {
+                     task: action.payload,
+                     id: Date.now(),
+                     completed: false
+                 }]};
+                 case 'REMOVE_TASK':
+                     return {todoList: state.todoList.filter(task => task.id !== action.payload)};
+                     case 'REMOVE_COMPLETED':
+                         return {todoList: state.todoList.filter(task => task.completed === false)};
+                         default:
+                            return state;
+          }        
+        }
+       
